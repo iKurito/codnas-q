e6a1639c84401efa982cb542e570b2f60353550d
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-07-25 17:22:12 -0500

    minor fixes

--- codnas-q-react/src/pages/About/index.js
+++ codnas-q-react/src/pages/About/index.js
@@ -4,7 +4,7 @@ import Image from '../../assets/img/about-img.png'
 const About = () => {
   return (
     <Fragment>
-      <div className="pt-6 pb-60">
+      <div className="pt-6 pb-52">
         <div className="px-4 sm:px-16 md:px-24 lg:px-48">
           <div className="p-5 sm:p-10 space-y-4">
             <h1 className="text-gray-700 text-3xl md:text-4xl font-bold text-center">About</h1>
--- codnas-q-react/src/pages/Contact/index.js
+++ codnas-q-react/src/pages/Contact/index.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const Contact = () => {
   return (
     <Fragment>
-      <div className="pt-6 pb-60">
+      <div className="pt-6 pb-52">
         <div className="px-4 sm:px-16 md:px-24 lg:px-48">
           <div className="p-5 sm:p-10 space-y-4 text-center">
             <h1 className="text-gray-700 text-3xl md:text-4xl font-bold">Contact Us</h1>
