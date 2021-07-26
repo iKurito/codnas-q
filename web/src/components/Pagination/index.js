5feef443c5fd3d6a64441267714b2310d4ddeb21
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-07-26 07:55:39 -0500

    resultas loaded

---
+++ web/src/components/Pagination/index.js
@@ -0,0 +1,81 @@
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import { usePagination, DOTS } from '../../hooks/usePagination'
+
+const Pagination = (props) => {
+  const { onPageChange, totalCount, siblingCount = 1, currentPage, pageSize, className } = props
+
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  })
+
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1)
+  }
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1)
+  }
+
+  const lastPage = paginationRange[paginationRange.length - 1]
+
+  return (
+    <ul className={classnames('pagination-container justify-center', { [className]: className })}>
+      <li
+        className={classnames('pagination-item', {
+          disabled: currentPage === 1,
+        })}
+        onClick={onPrevious}
+      >
+        <div className="arrow left" />
+      </li>
+      {paginationRange.map((pageNumber) => {
+        if (pageNumber === DOTS) {
+          return (
+            <li key={pageNumber} className="pagination-item dots">
+              &#8230;
+            </li>
+          )
+        }
+
+        return (
+          <li
+            key={pageNumber}
+            className={classnames('pagination-item', {
+              selected: pageNumber === currentPage,
+            })}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </li>
+        )
+      })}
+      <li
+        className={classnames('pagination-item', {
+          disabled: currentPage === lastPage,
+        })}
+        onClick={onNext}
+      >
+        <div className="arrow right" />
+      </li>
+    </ul>
+  )
+}
+
+Pagination.propTypes = {
+  onPageChange: PropTypes.any.isRequired,
+  totalCount: PropTypes.any.isRequired,
+  siblingCount: PropTypes.any,
+  currentPage: PropTypes.any.isRequired,
+  pageSize: PropTypes.any.isRequired,
+  className: PropTypes.any.isRequired,
+}
+
+export default Pagination
--- web/src/pages/AdvSearch/Result/index.js
+++ web/src/pages/AdvSearch/Result/index.js
@@ -1,15 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import ContentLoader from 'react-content-loader'
+import Pagination from '../../../components/Pagination'
+import { data } from './data'
+import Protein from '../../../assets/img/protein.png'
 
 const skeleton = [1, 2, 3, 4, 5, 6]
 
+const PageSize = 10
+
 const Result = () => {
+  const [currentPage, setCurrentPage] = useState(1)
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(true), 1000)
     return () => clearTimeout(timer)
   }, [])
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize
+    const lastPageIndex = firstPageIndex + PageSize
+    return data.slice(firstPageIndex, lastPageIndex)
+  }, [currentPage])
 
   return (
     <section className="pt-4 lg:pt-0 md:col-span-3">
@@ -41,7 +53,52 @@ const Result = () => {
             ))}
           </div>
         ) : (
-          <h1>Aqui poner resultados</h1>
+          <>
+            <Pagination
+              className="pagination-bar"
+              currentPage={currentPage}
+              totalCount={data.length}
+              pageSize={PageSize}
+              onPageChange={(page) => setCurrentPage(page)}
+            />
+            <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-6 py-5">
+              {currentTableData.map((item) => {
+                return (
+                  <div
+                    key={item.id}
+                    className="col-span-1 mx-auto custom-shadow p-4 w-full  cursor-pointer"
+                    onClick={() => console.log(item.id)}
+                  >
+                    <h1>Cluster {item.id}</h1>
+                    <hr />
+                    <div className="grid grid-cols-2">
+                      <div className="self-center">
+                        <h1>Group: {item.group}</h1>
+                        <hr />
+                        <h1>Oligomeric State: {item.oligomeric}</h1>
+                        <hr />
+                        <h1>Conformers Quantity: {item.num}</h1>
+                        <hr />
+                        <h1>Max RMSD Quaternary: {item.quat}</h1>
+                        <hr />
+                        <h1>Max RMSD Tertiary: {item.ter}</h1>
+                      </div>
+                      <div className="slef-center">
+                        <img className="mx-auto" src={Protein} alt="protein" />
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+            <Pagination
+              className="pagination-bar"
+              currentPage={currentPage}
+              totalCount={data.length}
+              pageSize={PageSize}
+              onPageChange={(page) => setCurrentPage(page)}
+            />
+          </>
         )}
       </div>
     </section>
