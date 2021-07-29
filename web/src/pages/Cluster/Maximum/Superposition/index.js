042f14c69c7fad253c32c7ed4948511dd84c3c0e
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-07-29 16:19:23 -0500

    minor fixes

--- web/src/pages/Cluster/Maximum/Superposition/index.js
+++ web/src/pages/Cluster/Maximum/Superposition/index.js
@@ -1,6 +1,7 @@
 import { Fragment, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import ReactLoading from 'react-loading'
+import Brightness1Icon from '@material-ui/icons/Brightness1'
 
 const Superposition = ({ query, target }) => {
   const [loaded, setLoaded] = useState(false)
@@ -48,9 +49,42 @@ const Superposition = ({ query, target }) => {
 
   return (
     <Fragment>
-      <div className="p-4 justify-self-center">
+      <div className="p-4 justify-self-center" style={{ textAlign: '-webkit-center' }}>
         {loaded ? (
-          <div id="viewport" className="h-80 w-40 lg:w-96" onWheel={() => onWheel()} />
+          <>
+            <div id="viewport" className="h-96 w-40 lg:w-96" onWheel={() => onWheel()} />
+            <span>View superposed coordinates files</span>
+            <div className="grid grid-cols-2 gap-2 sm:gap-4">
+              <div>
+                <Brightness1Icon
+                  fontSize="small"
+                  style={{ color: 'red', marginRight: '1vh', marginBottom: '0.5vh' }}
+                />
+                <a
+                  className="text-primary-original hover:text-primary-dark"
+                  href={`https://codnasq.herokuapp.com/pdb/${query}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="text-xs sm:text-lg">{query} (query)</span>
+                </a>
+              </div>
+              <div>
+                <Brightness1Icon
+                  fontSize="small"
+                  style={{ color: 'green', marginRight: '1vh', marginBottom: '0.5vh' }}
+                />
+                <a
+                  className="text-primary-original hover:text-primary-dark"
+                  href={`https://codnasq.herokuapp.com/pdb/${target}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="text-xs sm:text-lg">{target} (target)</span>
+                </a>
+              </div>
+            </div>
+          </>
         ) : (
           <div id="loader">
             <ReactLoading type="spin" color="#2d699b" />
