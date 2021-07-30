b0019bb900ce2fe67c5d5aab8d1f9d11306aa0d7
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-07-30 03:23:54 -0500

    minor fix

--- service/src/main/java/codnas/q/service/data/repository/IConformerDAO.java
+++ service/src/main/java/codnas/q/service/data/repository/IConformerDAO.java
@@ -11,7 +11,7 @@ public interface IConformerDAO extends JpaRepository<Conformer, Integer> {
     @Query(value = "SELECT * from conformer where cluster_id = :cluster_id", nativeQuery = true)
     List<Conformer> getAllConformersByClusterId(@Param("cluster_id") String cluster_id);
 
-    @Query(value = "SELECT * from conformer where pdb_id = :pdb_id", nativeQuery = true)
+    @Query(value = "SELECT * from conformer where pdb_id = :pdb_id limit 1", nativeQuery = true)
     Conformer getConformerById(@Param("pdb_id") String pdb_id);
 
     @Query(value = "SELECT * from conformer where name like concat('%', :name, '%');", nativeQuery = true)
