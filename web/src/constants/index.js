9d200858929dafe552a48009543c2e7fda1e6752
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-08-09 10:06:14 -0500

    contact us finished

--- web/src/constants/index.js
+++ web/src/constants/index.js
@@ -1,1 +1,2 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
+export const MAPS_KEY = 'AIzaSyDaMocA4Fuc9FbzvgcYuYwqfg_43q1U2zE'
---
+++ web/src/credentials/index.js
@@ -0,0 +1,3 @@
+export default {
+  mapsKey: 'AIzaSyDaMocA4Fuc9FbzvgcYuYwqfg_43q1U2zE',
+}
